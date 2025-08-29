a25c35514d38435e4a43c15e9da3ddab0ad79cfd
Vishnu Tejas E <vishnutejase@gmail.com>
2025-08-30 01:04:57 +0530

    Fix url

--- frontend/src/shared/services/water-api.ts
+++ frontend/src/shared/services/water-api.ts
@@ -1,6 +1,6 @@
 import { PredictionInputData, WaterInputData } from '../types'
 
-const API_BASE_URL = 'http://aigis-backend.azurewebsites.net'
+const API_BASE_URL = 'https://aigis-backend.azurewebsites.net'
 
 export interface ServerAnalysisResponse {
   quality_analysis: string
